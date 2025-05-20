cd33c8042bbbe415a4104fe625c9e2a813b2201c
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-05-20 13:53:03 +0200

    created files required for the ex02

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 13:52:08 by paprzyby          #+#    #+#             */
+/*   Updated: 2025/05/20 13:52:48 by paprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe()	{}
+
+PmergeMe::PmergeMe(const PmergeMe &other)	{}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+	if (this != &other)
+	{
+		//
+	}
+	return (*this);
+}
+
+PmergeMe::~PmergeMe()	{}
---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 13:51:34 by paprzyby          #+#    #+#             */
+/*   Updated: 2025/05/20 13:52:04 by paprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+
+class	PmergeMe
+{
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe &other);
+		PmergeMe &operator=(const PmergeMe &other);
+		~PmergeMe();
+};
+
+#endif
