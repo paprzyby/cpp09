c382063b1acd86ec94ca40e0e9afc84f2c14d834
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-08-24 17:09:38 +0200

    finished function for parsing the data

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:52:08 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/08/24 16:39:58 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/08/24 17:09:26 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 PmergeMe::PmergeMe()	{}
 
-PmergeMe::PmergeMe(const PmergeMe &other)	{}
+PmergeMe::PmergeMe(const PmergeMe &other)
+{
+	(void)other;
+}
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 {
@@ -29,5 +32,26 @@ PmergeMe::~PmergeMe()	{}
 
 void	PmergeMe::parsingData(char **av)
 {
-	
+	long long	num;
+
+	for (int i = 1; av[i]; i++)
+	{
+		try
+		{
+			num = std::stoll(av[i]);
+		}
+		catch(const std::exception& e)
+		{
+			throw std::runtime_error("Error: Only numbers are allowed");
+		}
+		if (num < 0)
+		{
+			throw std::runtime_error("Error: Only positive numbers are allowed");
+		}
+		else if (num > INT_MAX || num < INT_MIN)
+		{
+			throw std::runtime_error("Error: Only numbers that fit in an integer are allowed");
+		}
+		input.push_back(static_cast<int>(num));
+	}
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:51:34 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/08/24 16:38:21 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/08/24 16:45:04 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <deque>
 # include <list>
 # include <algorithm>
+# include <limits>
 
 class	PmergeMe
 {
@@ -29,8 +30,8 @@ class	PmergeMe
 		void	parsingData(char **av);
 
 	private:
-		std::deque<int> input;
-		std::list<int> sorted;
+		std::deque<int>	input;
+		std::list<int>	sorted;
 };
 
 #endif
